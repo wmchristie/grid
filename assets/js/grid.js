84fbc0cb5704195120bf01fee139166b0d97fceb
Bill Christie <william.christie@gmail.com>
2012-05-21 18:35:22 -0400

    fixed bugs

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -3,128 +3,101 @@
     _.templateSettings.variable = 'item';
 
     app.gridTemplates = {
-        value : _.template('<td class="col<%= item.c %>"><div class="data"><span><%= item.value %></a></span></div></td>'),
-        rowLabel : _.template('<td class="branch" colspan="<%= item.span %>"><div class="data"><span title="<%= item.value.replace(this.titleRegex, "") %>"><%= item.value %></span></div></td>'),
+        value : _.template('<td class="col<%= item.c %>"><div class="data"><span title="<%= item.value.replace(item.regex, "") %>"><%= item.value %></span></div></td>'),
+        anchor : _.template('<td class="col<%= item.c %>"><div class="data"><span><a href="" title="<%= item.value.replace(item.regex, "") %>"><%= item.value %></a></span></div></td>'),
+        rowLabel : _.template('<td class="branch" colspan="<%= item.colspan %>"><div class="data"><span title="<%= item.value.replace(item.regex, "") %>"><%= item.value %></span></div></td>'),
         twisty : _.template('<td class="twisty"><div class="data"><span><button type="button" data-record="<%= item.row %>" class="<%= item.state %> image twisty"><div class="body"><div class="west side"></div><div class="east side"></div><b></b></div></button></span></div></td>'),
-        blank : _.template('<td class="col<%= item.c %>" colspan=""><div class="data"><span></span></div></td>')
+        blank : _.template('<td class="col<%= item.c %>"><div class="data"><span></span></div></td>')
     };
 
-    app.LabelRow = function (item, offset) {
+    app.createRow = function (item, offset) {
 
-        this.offset = offset;
-        this.value = item.value;
-        this.values = item.values;
-        this.max = this.offset + this.values.length;
+        var value = item.value,
+            values = item.values,
+            max = offset + values.length,
 
-    };
+            hasRowLabel = value.length > 0,
 
-    app.LabelRow.prototype = {
+            min = Math.min,
+            
+            label = app.gridTemplates.rowLabel,
+            before = app.gridTemplates.blank,
+            during = app.gridTemplates.value,
+            after = app.gridTemplates.blank,
 
-        titleRegex : /<[a-z\/][^>]*>/g,
+            titleRegex = /<[a-z\/][^>]*>/g;
 
-        before : app.gridTemplates.rowLabel,
-        template : app.gridTemplates.value,
+        return {
 
-        col : function (i, count) {
+            col : function (i, count) {
 
-            var result = '',
-                max = this.max, 
-                offset = this.offset,
-                template = this.template,
-                values = this.values,
-                limit,
-                v;
+                var result = '',
+                    template,
+                    maxLimit = i + count,
+                    colspan,
+                    v;
 
-            if (i < this.offset) {
+                if (i === 0 && hasRowLabel) { // todo : how do we set the total width of the colspan'ed cells when only row labels are being displayed
 
-                result = this.before({ 
-                    span : (i + count) > offset ? offset - i : count,
-                    value : i === 0 ? this.value : ''
-                });
+                    colspan = min(count, offset - i);
 
-            } 
+                    result = label({ 
+                        regex : titleRegex,
+                        colspan : colspan,
+                        value : value
+                    });
 
-            i += count;
-            v = i - offset;
-            limit = Math.min(max, count);
+                    i += colspan;
 
-            while (i < max && i < count) {
+                } 
 
-                result += template({
-                    c : i++,
-                    value : values[v++]
-                });
+                if (i >= maxLimit) {
+                    return result;
+                }
+
+                limit = min(offset, maxLimit);
+                template = before;
+
+                while (i < limit) { 
+                    result += template({ c : i++ });
+                }
+
+                if (i >= maxLimit) {
+                    return result;
+                }
+
+                v = i - offset;
+                limit = min(max, maxLimit);
+                template = during;
+
+                while (i < limit) {
+
+                    result += template({
+                        regex : titleRegex,
+                        c : i++,
+                        value : values[v++]
+                    });
+
+                }
+
+                if (i >= maxLimit) {
+                    return result;
+                }
+
+                template = after;
+
+                while (i < maxLimit) {
+                    result += template({ c : i++ });
+                }
+
+                return result;
 
             }
 
-            template = this.after;
 
-            while (i < count) {
-                result += template({ c : i++ });
-            }
-
-            return result;
-
-        }
+        };
 
     };
-
-    app.Row = function (item, offset) {
-
-        this.offset = offset;
-        this.value = item.value;
-        this.values = item.values;
-        this.max = this.offset + this.values.length;
-
-    };
-
-    app.Row.prototype = {
-
-        titleRegex : /<[a-z\/][^>]*>/g,
-
-        before : app.gridTemplates.blank,
-        after : app.gridTemplates.blank,
-        template : app.gridTemplates.value,
-
-        col : function (i, count) {
-
-            var result = '',
-                max = this.max, 
-                offset = this.offset,
-                template = this.before,
-                values = this.values,
-                v,
-                limit = Math.min(offset, count);
-
-            while (i < limit) { // we can reduce this to a single test using Math.min()
-                result += template({ c : i++ });
-            }
-
-            template = this.template;
-            v = i - offset;
-            limit = Math.min(max, count);
-
-            while (i < limit) {
-
-                result += template({
-                    c : i++,
-                    value : values[v++]
-                });
-
-            }
-
-            template = this.after;
-
-            while (i < count) {
-                result += template({ c : i++ });
-            }
-
-            return result;
-
-        }
-
-    };
-
 
     app.RowFactory = function () {};
     app.RowFactory.prototype = {
@@ -146,7 +119,7 @@
 
                 record = records[i];
 
-                result[this.i++] = new (record.value.length > 0 ? app.LabelRow : app.Row)(record, offset);
+                result[this.i++] = app.createRow(record, offset);
 
                 if (record.nodes) {
                     this.addRecords(record.nodes, nextOffset, result);
@@ -170,38 +143,36 @@
 
     };
 
-    foo = function (markup, tbody, table) {
+    writeTable = function (markup, table) {
 
-        var m = '<table>';
+        var m = '<table>',
+            body = table.childNodes[0];
+
         m += markup;
         m += '</table>'
 
         var div = document.createElement('div');
 
         div.innerHTML = m;
-        return table.replaceChild(div.firstChild.firstChild, table.firstChild);
+
+        var newBody = div.childNodes[0].childNodes[0];
+
+        return table.replaceChild(newBody, body);
 
     };
 
+    var table = $('table')[0];
 
-
-    $(function () {
-
-        var body = $('tbody')[0];
-        var table = $('table')[0];
-
-        var markupTime;
-        var insertTime;
-
-        var factory = new app.RowFactory();
-
-        var rows = factory.create(app.data.records);
+    var show = function (rows, colStart) {
 
         var start = new Date();
 
         var list = '';
 
-        for (var i = 0; i < rows.length; i++) {
+        var rowCount = 21;
+        var colCount = 5;
+
+        for (var i = 0; i < rowCount; i++) {
 
             list += '<tr';
 
@@ -211,7 +182,7 @@
 
             list += '>';
 
-            list += rows[i].col(0, 8);
+            list += rows[i].col(colStart, colCount);
 
             list += '</tr>';
 
@@ -221,11 +192,31 @@
 
         start = new Date();
 
-        body = foo(list, body, table);
+        writeTable(list, table);
 
         insertTime = new Date().getTime() - start.getTime();
 
         console.log(markupTime, ' ', insertTime, ' ', markupTime + insertTime);
+
+    };
+
+
+    $(function () {
+
+        var markupTime;
+        var insertTime;
+
+        var factory = new app.RowFactory();
+
+        var rows = factory.create(app.data.records);
+
+        var colStart = 0;
+
+        show(rows, colStart++);
+
+        $('#by_cell').on('click', function () {
+            show(rows, colStart++);
+        });
 
     });
 
