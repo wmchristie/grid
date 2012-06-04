55a3ce21ed72e71bc1bba7f45f73a102470ef883
Bill Christie <william.christie@gmail.com>
2012-06-04 18:53:19 -0400

    Added destroy() and update() methods to gridCss with corresponding unit tests.

--- assets/js/gridcss.js
+++ assets/js/gridcss.js
@@ -11,68 +11,29 @@
             cssWriter,
             template = app.gridTemplates.gridCss;
 
-        cssWriter = app.cssWriter(template({
-            gridId : gridId,
-            columns : columns.getWidthInfos()
-        }));
-
-        return {
-            rules : cssWriter.rules
-        };
-    };
-
-    app.CssRuleBuilder = function () {
-        this.clear();
-    };
-
-    app.CssRuleBuilder.prototype = {
-
-        _rules : {},
-        _additionalRules : '',
-
-        additionalRules : function () {
-            return this._additionalRules;
-        },
-
-        addRuleFactory : function (name, factory) {
-            this._rules[name] = factory;
-        },
-
-        clear : function () {
-            this._rules = {};
-            delete this._selectorFactory;
-        },
-
-        rules : function (item) {
-
-            var rules = '';
-
-            _.each(this._rules, function (factory, key) {
-                rules += key + ': ' + factory(item) + '; ';
+        function createMarkup(cols) {
+            return template({
+                gridId : gridId,
+                columns : cols.getWidthInfos()
             });
-
-            return rules;
-
-        },
-
-        selector : function (item) {
-            return this._selectorFactory(item);
-        },
-
-        setAdditionalRules : function (rules) {
-            this._additionalRules = rules;
-        },
-
-        setSelectorFactory : function (factory) {
-            this._selectorFactory = factory;
         }
 
+        cssWriter = app.cssWriter(createMarkup(columns));
+
+        return {
+
+            id : cssWriter.id,
+            rules : cssWriter.rules,
+
+            destroy : function () {
+                cssWriter.destroy();
+            },
+
+            update : function (columns) {
+                cssWriter.update(createMarkup(columns));
+                this.rules = cssWriter.rules;
+            }
+        };
     };
-/*
-                '<% _.each(data.items, function (item) { %>' +
-                '    <%= this.selector(item) %> { <%= this.rules(item) %>}\r\n' +
-                '<% }, this); %>' +
-                '<%= this.additionalRules() %>' +
-*/
 
 }(window.app || (window.app = {}), jQuery, _));
--- test/unit/gridcss.js
+++ test/unit/gridcss.js
@@ -4,38 +4,115 @@ $(function () {
 
     module('gridCss');
 
-    test('should create a width rule for each column', function () {
+    var gridId = 'grid0',
+        css,
+        infos;
 
-        var gridId = 'grid0',
-            css,
-            infos;
+    function execute() {
 
         infos = [
             { width : 100, align : 'left', index : 0},
             { width : 42, align : 'right', index : 1 }
         ];
-            
-        css = app.gridCss(gridId, {
+
+        return app.gridCss(gridId, {
             getWidthInfos : function () {
                 return  infos;
             }
         });
 
+    }
+
+    test('should add the stylesheet to the page and expose the id', function () {
+
+        css = execute();
+
+        ok($('head #' + css.id).length === 1, 'expected stylesheet with id of "' + css.id + '" to exist on the page');
+
+        css.destroy();
+    });
+            
+    test('should remove the stylesheet from the page when destroy is called', function () {
+
+        css = execute();
+        css.destroy();
+
+        ok($('#' + css.id).length === 0, 'expected stylesheet with id of "' + css.id + '" to be removed from the page');
+
+    });
+            
+    test('should create a width rule for each column', function () {
+
+        css = execute();
+
         _.each(infos, function (info, i) {
 
-            var rule = css.rules[i], 
-                style = rule.style,
-                width = style.width,
-                align = style.textAlign;
+            var width = css.rules[i].style.width;
 
             ok(width === info.width + 'px', 'expected ' + info.width + 'px but was ' + width);
+
+        });
+
+        css.destroy();
+
+    });
+
+    test('should create a text-align rule for each column', function () {
+
+        css = execute();
+
+        _.each(infos, function (info, i) {
+
+            var align = css.rules[i].style.textAlign;
+
             ok(align === info.align, 'expected ' + info.align + ' but was ' + align);
+
+        });
+
+        css.destroy();
+
+    });
+
+    test('should begin each selector with the gridId', function () {
+
+        css = execute();
+
+        _.each(infos, function (info, i) {
+
+            var rule = css.rules[i]; 
 
             ok(rule.cssText.indexOf('#' + gridId) === 0, 'expected the selector to start with "#' + gridId + '"');
 
         });
+
+        css.destroy();
+
     });
 
+    test('should update the styles with the updated values on the columns', function () {
 
+        css = execute();
+
+        infos[0].width += 10;
+        infos[0].width += 20;
+
+        css.update({
+            getWidthInfos : function () {
+                return  infos;
+            }
+        });
+        
+        _.each(infos, function (info, i) {
+
+            var width = css.rules[i].style.width;
+
+            ok(width === info.width + 'px', 'expected ' + info.width + 'px but was ' + width);
+
+        });
+
+        css.destroy();
+
+    });
+    
 
 });
