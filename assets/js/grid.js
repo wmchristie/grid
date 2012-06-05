67b0edb71ea3df73766ab2151eef36aaa92ebb5e
Bill Christie <william.christie@gmail.com>
2012-06-05 18:04:04 -0400

    deleted left over templates from grid.js

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -3,38 +3,6 @@
 
     _.templateSettings.variable = 'item';
 
-    app.gridTemplates = {
-        value : _.template('<td class="col<%= item.c %>"><div class="data"><span title="<%= item.value.replace(item.regex, "") %>"><%= item.value %></span></div></td>'),
-        anchor : _.template('<td class="col<%= item.c %>"><div class="data"><span><a href="" title="<%= item.value.replace(item.regex, "") %>"><%= item.value %></a></span></div></td>'),
-        rowLabel : _.template('<td class="branch" colspan="<%= item.colspan %>"><div class="data"><span title="<%= item.value.replace(item.regex, "") %>"><%= item.value %></span></div></td>'),
-        twisty : _.template('<td class="twisty"><div class="data"><span><button type="button" data-record="<%= item.row %>" class="<%= item.state %> image twisty"><div class="body"><div class="west side"></div><div class="east side"></div><b></b></div></button></span></div></td>'),
-        blank : _.template('<td class="col<%= item.c %>"><div class="data"><span></span></div></td>'),
-
-        head : _.template(
-            '<th class="col<%= item.ordinal %><%= item.sizable %><%= item.sortable %><%= item.sort %>">' +
-            '    <div class="cell">' +
-            '        <div class="side west"></div>' +
-            '        <div class="data">' +
-            '            <span>' +
-            '                <span><%= item.name %></span>' +
-            '                <div class="sort-indicator"></div>' +
-            '                <div class="sort-order-indicator"><%= item.sortIndicator %></div>' +
-            '            </span>' +
-            '        </div>' +
-            '        <div class="side east"></div>' +
-            '    </div>' +
-            '</th>'
-        ),
-
-        headGroup : _.template('<th></th>'),
-
-        gridCss : _.template(
-            '<% _.each(item.columns, function (col) { %>' +
-            '   #<%= item.gridId %> .col<%= col.index %> .data { width: <%= col.width %>; }' +
-            '<% } %>'
-        )
-    };
-
     $(function () {
 
         var markupTime,
